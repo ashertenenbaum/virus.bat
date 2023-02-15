55f8c769ff921fc1212370e2a26a7345447c9f77
Asher T <100488394+TheIcySnicy@users.noreply.github.com>
2023-02-15 00:22:24 -0800

    Update Shutdown.bat
    keep in mind i have set the timer to 30 seconds (so there is enough time to open the cmd prompt and type "shutdown -a" to abort the shutdown incase they have some unsaved files. If you want it to be instant, change the timer from 30 secs to 1 sec)

--- Shutdown.bat
+++ Shutdown.bat
@@ -2,4 +2,4 @@
 cmd /k shutdown -s -t 30 /c hahahahaha 
 
 
-// keep in mind i have set the timer to 30 secconds (so there is enough time to open the cmd prompt and type "shutdown -a" to abort the shutdown incase they have some unsaved files. If you want it to be instant, change the timer frokm 30 secs to 1 sec)
+
